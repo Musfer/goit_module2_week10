ce6ce66574732f03f327e351b7a1fe439e0b51ca
Musfer <adzhymambetov@gmail.com>
2022-12-04 15:46:27 +0200

    updates in Pipfiles. Added factorial module

--- factorial.py
+++ factorial.py
@@ -1,1 +1,18 @@
-docker run --name redis-cache -d redis -p 6379:6379
+import redis
+from redis_lru import RedisLRU
+
+client = redis.StrictRedis(host="localhost", port=6379, password=None)
+cache = RedisLRU(client)
+
+
+@cache
+def fibonacci(x):
+    print(f"Calling f({x})")
+    if x in (0, 1):
+        return 1
+    return fibonacci(x-1) + fibonacci(x-2)
+
+
+if __name__ == "__main__":
+    for i in range(50):
+        print(f"fibonacci({i})={fibonacci(i)}")
